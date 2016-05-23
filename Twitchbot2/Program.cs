455b581dde9e8e070d2a0efec716d1fb250e772c
Michaels GPC <Michaels GPC@MICHAELSGPC>
2016-05-22 18:38:02 -0700

    Add project files.

---
+++ Twitchbot2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitchbot2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IrcBot irc = new IrcBot("irc.twitch.tv", 6667,
+                "bauwsbot", "oauth:yk1erggcp1egwf527jrwouu7molbp1");
+            irc.joinRoom("bauwsbot");
+            while (true)
+            {
+                string message = irc.readMessage();
+                if (message.Contains("!test"))
+                {
+                    irc.sendChatMessage("Testing requested.");
+                }
+            }
+        }
+    }
+}
